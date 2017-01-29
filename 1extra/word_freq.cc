7c134843e4452d1ea1f64987888de95acd6de611
cassvin <ccc6710@163.com>
2017-01-29 23:05:09 +0800

    add

---
+++ 1extra/word_freq.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct Word
+{
+	std::string word;
+	int freq;
+};
+
+class WordStatic
+{
+	public:
+		void read_file(const std::string& filename);
+		void write_file(const std::string& filename);
+	private:
+		std::vector<struct Word> word_vector;
+};
+
+void WordStatic::read_file(const std::string& filename)
+{
+	std::ifstream ifs(filename.c_str());
+	if(ifs.good()==0)
+	{
+		std::cout<<"open readfile failed"<<std::endl;
+		return;
+	}
+	std::string str;
+	std::vector<struct Word>::iterator it;
+	while(ifs>>str)
+	{
+		for(it==word_vector.begin();it!=word_vector.end();++it)
+		{
+			if(str.compare((*it).word)==0)
+			{
+				++((*it).freq);
+			}
+			else{
+				Word new_word;
+				new_word.word=str;
+				new_word.freq=1;
+				word_vector.push_back(new_word);
+			}
+		}
+	}
+	for(it==word_vector.begin();it!=word_vector.end();++it)
+	{
+		std::cout<<"word is "<<(*it).word<<",freq is"<<(*it).freq<<std::endl;
+	}
+}
+
+void WordStatic::write_file(const std::string& filename)
+{
+
+}
+
+int main(int argc,char* argv[])
+{
+	if(argc!=2)
+	{
+		std::cout<<"error args!"<<std::endl;
+	}
+	WordStatic ws;
+	ws.read_file(argv[1]);
+	return 0;
+}
